d5728471462e37d2745609d391461e515c47b104
phatnguyen1604 <phat.nguyen1604@hcmut.edu.vn>
2022-10-18 21:59:25 +0700

    lab 2 ex 1

---
+++ Core/Inc/timer.h
@@ -0,0 +1,15 @@
+/*
+ * timer.h
+ *
+ *  Created on: Oct 3, 2022
+ *      Author: 84977
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+extern int timer1_flag;
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_TIMER_H_ */
